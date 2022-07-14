b2e21a5474ba2972da725062757d91a51e6c1591
June Yoo <juneyo0823@naver.com>
2022-07-14 16:25:28 +0900

    [수학,사칙연산]BJ_1247_부호

--- 202207/[M,R]_[BJ_1247_부호]_[수학,사칙연산].py
+++ 202207/[M,R]_[BJ_1247_부호]_[수학,사칙연산].py
@@ -1,3 +1,9 @@
+# BJ 1247 부호 <수학,사칙연산,임의 정밀도 / 큰 수 연산>
+# https://www.acmicpc.net/problem/1247
+# 시간 : 248ms
+# 문제 리뷰 : M , R
+# 회고 : 빠르게 입력받는 sys.stdin.readline() , 연속적으로 숫자를 입력받을 때 반복문+리스트로 한 줄로 입력 가능
+
 import sys
 
 for i in range(3):
