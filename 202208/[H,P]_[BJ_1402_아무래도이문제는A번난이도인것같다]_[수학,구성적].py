b5cbb70bf083f45f291e322d79421de54ab3bc23
June Yoo <juneyo0823@naver.com>
2022-08-18 22:52:06 +0900

    [수학,구성적]BJ_1402_아무래도이문제는A번난이도인것같다

---
+++ 202208/[H,P]_[BJ_1402_아무래도이문제는A번난이도인것같다]_[수학,구성적].py
@@ -0,0 +1,9 @@
+# BJ 1402 아무래도이문제는A번난이도인것같다 <수학,구성적>
+# https://www.acmicpc.net/problem/1402
+# 시간 : 84 ms
+# 문제 리뷰 : H , P
+# 회고 :
+
+for i in range(int(input())):
+    A,B = map(int, input().split())
+    print('yes')
